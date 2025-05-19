89877f69190e8571d5aa5061c34e3a0eeec3469d
sam-sprague <samuel.e.sprague@gmail.com>
2025-05-19 01:21:18 -0400

    updated player.py and constants.py to limit fire rate

--- player.py
+++ player.py
@@ -8,6 +8,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shot_timer = 0
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
@@ -21,6 +22,7 @@ class Player(CircleShape):
         return [a, b, c]
 
     def update(self, dt):
+        self.shot_timer -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -43,6 +45,9 @@ class Player(CircleShape):
         self.position += distance
 
     def shoot(self):
-        shot = Shot(self.position)
+        if self.timer > 0:
+            return
+        self.shot_timer = PLAYER_SHOOT_COOLDOWN
+        shot = Shot(self.position.x, self.position.y)
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         shot.velocity = forward * PLAYER_SHOOT_SPEED
