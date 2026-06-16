1501ff87368351662e79e49c73f963f449adbd76
sam-sprague <samuel.e.sprague@gmail.com>
2026-06-16 16:25:22 -0400

    Add FPS limiting and delta time calculation

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 
 
-def main():
+def main() -> None:
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0.0
 
     while True:
         log_state()
@@ -16,6 +18,9 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
