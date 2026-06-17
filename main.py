b4a0d442491f9b8db43dc61ba843907ad71a2b56
sam-sprague <samuel.e.sprague@gmail.com>
2026-06-17 12:46:37 -0400

    Add bullet-asteroid collisions and destruction

--- main.py
+++ main.py
@@ -45,6 +45,12 @@ def main() -> None:
                 print("Game over!")
                 sys.exit()
 
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    asteroid.kill()
+
         screen.fill("black")
 
         for obj in drawable:
